952755066436f08cb3b448af95f4248d8890384f
Humayun Kabir <humayunkabir.cep@gmail.com>
2021-08-12 01:21:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const yahooFinance = require('yahoo-finance');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get('/:type/:symbols', (req, res) => {
+  if (['quote', 'historical'].includes(req.params.type)) {
+    const symbols = req.params.symbols.split(',');
+    const options = {
+      ...(symbols.length ? { symbols } : { symbol: symbols[0] }),
+      ...req.query,
+    };
+    yahooFinance[req.params.type](options)
+      .then((quotes) => res.send(quotes))
+      .catch((err) => res.status(404).send(err));
+  } else {
+    res.status(400).send({
+      status: 400,
+      message: "Bad Request: Type must be one of 'quote' or 'historical'!",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Finance app listening at http://localhost:${port}`);
+});
